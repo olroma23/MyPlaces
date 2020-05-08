f98e050f3c0748b1ee1a2d4ec5da32931af936e3
Roman Oliinyk <roliynyk68@gmail.com>
2020-05-08 19:08:00 +0300

    test

--- MyPlaces/MainTableVC.swift
+++ MyPlaces/MainTableVC.swift
@@ -41,7 +41,7 @@ class MainTableVC: UITableViewController {
         return cell
     }
 
-
+//sds
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
